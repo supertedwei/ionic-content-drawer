f1b0c4b271df3aca647ef811022eeafeb3f78cae
Ted Wei <jdwei@hotmail.com>
2017-02-28 07:25:45 +0800

    implement ContentDrawer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { ContentDrawer } from '../components/content-drawer/content-drawer';
+ 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ContentDrawer
   ],
   imports: [
     IonicModule.forRoot(MyApp)
